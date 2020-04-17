43512c15fbead19b33c9156f51fc8587b62c90a5
ana carolina sacco araujo <ana.araujo.ie@gmail.com>
2020-04-17 19:05:09 +0000

    404 bug

--- mobdev-ca2/src/app/sevices/api.service.ts
+++ mobdev-ca2/src/app/sevices/api.service.ts
@@ -14,4 +14,24 @@ export class ApiService {
     getEpisode(id) {
         return this.http.get('https://www.breakingbadapi.com/api/episodes/${id}');
     }
+    getCharacters() {
+        return this.http.get('https://breakingbadapi.com/api/characters');
+
+    }
+    getCharacter(id) {
+        return this.http.get('https://breakingbadapi.com/api/characters/${id}');
+    }
+
+    getQuotes() {
+        return this.http.get('https://breakingbadapi.com/api/quotes');
+
+    }
+    getQuote(id) {
+        return this.http.get('https://breakingbadapi.com/api/quotes/${id}');
+    }
+
+    getDeaths() {
+        return this.http.get('https://breakingbadapi.com/api/deaths');
+
+    }
 }
