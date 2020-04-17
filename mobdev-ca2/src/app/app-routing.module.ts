b52566f46ef93ccec799e8ee428dee1e1ae01694
ana carolina sacco araujo <ana.araujo.ie@gmail.com>
2020-04-17 16:59:41 +0000

    404 bug

--- mobdev-ca2/src/app/app-routing.module.ts
+++ mobdev-ca2/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+ 
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m=> m.TabsPageModule)},
