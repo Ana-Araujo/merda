1029703fbea7a0589fc4d01cce732645f0aa9a0f
ana carolina sacco araujo <ana.araujo.ie@gmail.com>
2020-04-18 15:44:51 +0000

    setting characters details

--- mobdev-ca2/src/app/pages/characters/characters.page.ts
+++ mobdev-ca2/src/app/pages/characters/characters.page.ts
@@ -2,26 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../sevices/api.service';
-
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.page.html',
   styleUrls: ['./characters.page.scss'],
 })
+
 export class CharactersPage implements OnInit {
 
     characters: Observable<any>;
 
     constructor(private router: Router, private api: ApiService) { }
   
-
   ngOnInit() {
       this.characters = this.api.getCharacters();
       this.characters.subscribe(data => {console.log('my data: ',data);
     });
   }
   openDetail(characters){
-      let charactersId = characters.characters_id;
+      let charId = characters.name;
       this.router.navigateByUrl('/tabs/characters/${charactersId}');
   }
 }
