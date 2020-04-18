ac7efc4eda3d273195555ccc192afd9e0c14b45f
ana carolina sacco araujo <ana.araujo.ie@gmail.com>
2020-04-18 15:56:28 +0000

    setting characters name list

--- mobdev-ca2/src/app/pages/characters/characters.page.ts
+++ mobdev-ca2/src/app/pages/characters/characters.page.ts
@@ -20,7 +20,7 @@ export class CharactersPage implements OnInit {
     });
   }
   openDetail(characters){
-      let charId = characters.name;
+      let char_id = characters.name;
       this.router.navigateByUrl('/tabs/characters/${charactersId}');
   }
 }
